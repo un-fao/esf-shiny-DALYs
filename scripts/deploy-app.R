43715a7d0775ab2ae37342b5667bc12ba03f7d59
Luc Warde <luc.warde@fao.org>
2025-11-27 16:46:38 +0100

    clean deploy app script

--- scripts/deploy-app.R
+++ scripts/deploy-app.R
@@ -14,23 +14,3 @@ rsconnect::deployApp(
   ),
   appName = "fao-food-safety-risk-categorization-model" #"esf-shiny-DALYs"
 )
-
-
-# For FAO internal server
-rsconnect::deployApp(
-  account = "luc.warde@fao.org",
-  server = "https://rstudio.fao.org",
-  appPrimaryDoc = "scripts/shiny.r",
-  appFiles = c(
-    "scripts/shiny.r",
-    "data/dalys_values.csv",
-    "data/risk_factors.csv"
-  ),
-  appName = "esf-shiny-DALYs"
-)
-
-# Possible servers:
-
-# https://rstudio.fao.org
-# https://rstudio.qa..fao.org
-# shinyapps.io
